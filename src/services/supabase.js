b02fe514215efeb48242213f846fed8e079db1eb
riliu <ilyukroma228@gmail.com>
2026-03-20 23:48:44 +0200

    feat: connect supabase with app

---
+++ src/services/supabase.js
@@ -0,0 +1,5 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://ltrpueouljjgppjljutm.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0cnB1ZW91bGpqZ3BwamxqdXRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4ODMzODEsImV4cCI6MjA4OTQ1OTM4MX0.B4MD4lgk1W-oHpRE_7L9nmRtgmmvMl3FYBVr7xxvgY0";
+const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase;

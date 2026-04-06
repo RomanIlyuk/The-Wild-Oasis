746f96b6a0624359299d2c4745fe7dd98c003acf
riliu <ilyukroma228@gmail.com>
2026-04-06 22:23:46 +0300

    feat: user sign up

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,5 +1,22 @@
 import supabase from "./supabase";
 
+export async function signup({ fullName, email, password }) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        fullName,
+        avatar: "",
+      },
+    },
+  });
+
+  if (error) throw new Error(error.message);
+
+  return data;
+}
+
 export async function login({ email, password }) {
   const { data, error } = await supabase.auth.signInWithPassword({
     email,
@@ -24,5 +41,4 @@ export async function getCurrentUser() {
 export async function logout() {
   const { error } = await supabase.auth.signOut();
   if (error) throw new Error(error.message);
-
 }

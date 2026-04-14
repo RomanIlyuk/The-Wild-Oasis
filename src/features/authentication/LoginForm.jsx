46e65713d750b77571f42293519fb2c6ae15e677
riliu <ilyukroma228@gmail.com>
2026-04-14 20:55:40 +0300

    fix: small bug with Login From

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import SpinnerMini from "../../ui/SpinnerMini";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-  const [email, setEmail] = useState("riliu@example.com");
-  const [password, setPassword] = useState("pass7777");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {

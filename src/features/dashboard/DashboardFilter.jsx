4b71759d46cd06ed5ab18be8a92137de5e0a02bd
riliu <ilyukroma228@gmail.com>
2026-04-14 20:07:45 +0300

    feat: display stays for current day

--- src/features/dashboard/DashboardFilter.jsx
+++ src/features/dashboard/DashboardFilter.jsx
@@ -1,13 +1,24 @@
-import Filter from '../../ui/Filter';
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import Filter from "../../ui/Filter";
 
 function DashboardFilter() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    if (!searchParams.get("last")) {
+      searchParams.set("last", "7");
+      setSearchParams(searchParams);
+    }
+  }, []);
+
   return (
     <Filter
-      filterField='last'
+      filterField="last"
       options={[
-        { value: '7', label: 'Last 7 days' },
-        { value: '30', label: 'Last 30 days' },
-        { value: '90', label: 'Last 90 days' },
+        { value: "7", label: "Last 7 days" },
+        { value: "30", label: "Last 30 days" },
+        { value: "90", label: "Last 90 days" },
       ]}
     />
   );
